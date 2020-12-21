d1063807ac6c8393d483b4763274d3b22f45d12d
joinmouse <908380434@qq.com>
2020-12-21 15:29:13 +0800

    冒泡排序及其优化

--- src/sort/Bubbling.java
+++ src/sort/Bubbling.java
@@ -6,17 +6,52 @@ package sort;
 // 3、重复2的操作，退出条件是剩余的数组n-i个已经遍历完成
 public class Bubbling {
     public int[] sort(int[] arr) {
+        int num = 0;  //比较次数
+        int swap = 0; //交换次数
         // 用于交换的临时数
         int temp = 0;
         for(int i=0; i<arr.length; i++) {
             for(int j=1; j<arr.length-i; j++) {
+                num += 1;
                 if(arr[j-1] > arr[j]) {
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    swap += 1;
                 }
             }
         }
+        System.out.println("简单冒泡排序比较次数: " + num);
+        System.out.println("简单冒泡排序交换次数: " + swap);
+        return arr;
+    }
+
+    public int[] optSort(int arr[]) {
+        int num = 0;  //比较次数
+        int swap = 0; //交换次数
+        int temp = 0;  //临时交换数
+        int flag = 0;
+        int len = arr.length;
+        int temPosition = 0;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=1; j < len; j++) {
+                num += 1;
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    flag = 1;
+                    temPosition = j;
+                    swap += 1;
+                }
+            }
+            len = temPosition;
+            if(flag == 0) {
+                return arr;
+            }
+        }
+        System.out.println("优化冒泡排序比较次数: " + num);
+        System.out.println("优化冒泡排序交换次数: " + swap);
         return arr;
     }
 }
--- src/test/sort/BubblingTest.java
+++ src/test/sort/BubblingTest.java
@@ -1,13 +1,30 @@
 package test.sort;
 
 import sort.Bubbling;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class BubblingTest {
     public static void main(String[] args) {
+        // 随机生成1000个数字
+        Random random = new Random();
+        int s1[] = new int[1000];
+        int s2[] = new int[1000];
+        for(int i=0; i< 1000; i++) {
+            int val = random.nextInt(1000);
+            s1[i] = val;
+            s2[i] = val;
+        }
+//        int s1[] = {2,3,5,6,1,11,12,13};
+//        int s2[] = {2,3,5,6,1,11,12,13};
         Bubbling bubbling = new Bubbling();
-        int[] arr = {1,4,12,2,5,7,8};
-        int[] result = bubbling.sort(arr);
-        System.out.println(Arrays.toString(result));
+        // 简单的冒泡排序
+        int[] result1 = bubbling.sort(s1);
+        System.out.println(Arrays.toString(result1));
+        // 优化的排序
+        int[] result2 = bubbling.optSort(s2);
+        System.out.println(Arrays.toString(result2));
     }
 }
